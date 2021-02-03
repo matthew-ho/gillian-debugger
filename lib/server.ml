8e436fdd881625914cd1b2ef7be5fcfcd7679ba1
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-02-03 20:49:07 +0000

    Add printing of parsed JSON

--- lib/server.ml
+++ lib/server.ml
@@ -1,5 +1,10 @@
 let rec loop () =
   let%lwt raw_json_res = Rpc.read_yojson () in
+  let () =
+      match raw_json_res with
+      | Ok json -> print_endline (Json.json_to_string json)
+      | Error s -> print_endline s
+  in
   loop ()
 
 let start_server () =
