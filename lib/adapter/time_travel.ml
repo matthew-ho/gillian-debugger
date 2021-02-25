07ee2d9bcd88890731a878ecd314cad6577e805e
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-02-25 17:34:56 +0000

    Implement continue request

--- lib/adapter/time_travel.ml
+++ lib/adapter/time_travel.ml
@@ -4,6 +4,35 @@ let run rpc =
   Lwt.pause ();%lwt
   Debug_rpc.set_command_handler
     rpc
+    (module Continue_command)
+    (fun _ ->
+      let () = Log.info "Continue request received" in
+      let stop_reason = Debugger.run () in
+      let _ =
+        match stop_reason with
+        | Debugger.Step ->
+          let () =
+            Log.info
+              "Debugger stopped because of step after running. This should not \
+               happen"
+          in
+          Lwt.return_unit
+        | Debugger.Exited ->
+          Lwt.return_unit
+          (* Do not send Terminated event to allow for stepping backwards *)
+        | Debugger.Uncaught_exc ->
+          Debug_rpc.send_event
+            rpc
+            (module Stopped_event)
+            Stopped_event.Payload.(
+              make
+                ~reason:Stopped_event.Payload.Reason.Exception
+                ~thread_id:(Some 0)
+                ())
+      in
+      Lwt.return (Continue_command.Result.make ()));
+  Debug_rpc.set_command_handler
+    rpc
     (module Next_command)
     (fun _ ->
       let () = Log.info "Next request received" in
